2f2c43d5836a2cc88690710a094389f1c965168a
Windisley <windisleyl@gmail.com>
2024-10-08 12:08:34 -0300

    Logica para buscar cep Pronta

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState} from 'react'
 import axios from "axios"
 
 import './App.css'
@@ -10,37 +10,80 @@ function App() {
   const [viacep, setViacep] = useState()
   const [loading, setLoading] = useState(false)
   const [err, setErr] = useState()
+  const [input, setInput] = useState("")
 
-  useEffect(() => {
-    axios.get("https://viacep.com.br/ws/01001000/json/").then((response) => {
-      const data = response.data
+   const searchcep = ()=>{
+    setLoading(true)
+    if(!input){
+      alert("Campo vazio informe um cep!")
+    }else{
+      axios.get(`https://viacep.com.br/ws/${input}/json/`).then((response) => {
+        const data = response.data
+        setViacep(data)
+        console.log(viacep)
+  
+      }).catch(()=>{
+        const error = console.error("Erro");
+        setErr(error)
+  
+      }).finally(()=>{
+        setLoading(false)
+        setInput("")
+      })
 
+    }
     
-      console.log(data)
-    })
-  }, [])
+   
+  }
+  
+         
+
+  
 
   return (
     <div className="container">
       <div className="boxviacep">
         <h1>via cep</h1>
         <div className="boxinput">
-          <input type="text" placeholder='Informe um cep'></input>
-          <button>buscar</button>
+          <input type="text" placeholder='Informe um cep' onChange={(iten)=>{setInput(iten.target.value)}} value={input}></input>
+          <button onClick={searchcep}>buscar</button>
         </div>
 
         <div className="boxinfo">
           <div>
-            <h2>estado:</h2>
-            <p>bairro</p>
+          <h2>estado:</h2>
+          {loading ? (
+              <p>Carregando...</p>
+            ) : err ? (
+              <p>{err}</p>
+            ) : (
+              <p>{viacep ? viacep.estado : "N/A"}</p>
+            )}
+ 
           </div>
+
           <div>
             <h2>bairro:</h2>
-            <p>bairro</p>
+            {loading ? (
+              <p>Carregando...</p>
+            ) : err ? (
+              <p>{err}</p>
+            ) : (
+              <p>{viacep ? viacep.localidade : "N/A"}</p>
+            )}
+ 
           </div>
+
           <div>
             <h2>rua:</h2>
-            <p>bairro</p>
+            {loading ? (
+              <p>Carregando...</p>
+            ) : err ? (
+              <p>{err}</p>
+            ) : (
+              <p>{viacep ? viacep.logradouro : "N/A"}</p>
+            )}
+ 
           </div>
         </div>
       </div>
